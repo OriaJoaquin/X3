9767d77b408ecb6d84a6a430067cd7f1069057d1
Joaquín Oria <joaquin.ariel.o@gmail.com>
2019-11-02 12:00:14 -0300

    Se refactorizo el codigo.

--- BrillaMe/Arduino/BrillaMe/BrillaMe/BrillaMe.ino
+++ BrillaMe/Arduino/BrillaMe/BrillaMe/BrillaMe.ino
@@ -14,7 +14,11 @@ int ldr4 = A3;
 int celda = A4;
 const char VALOR_INTENSIDAD = 4;
 const char CANT_MEDIDAS = 64;
-unsigned short dataldr[4][4][CANT_MEDIDAS] ={{{0}}};
+unsigned short dataldr1[4][CANT_MEDIDAS] ={{0}};
+unsigned short dataldr2[4][CANT_MEDIDAS] ={{0}};
+unsigned short dataldr3[4][CANT_MEDIDAS] ={{0}};
+unsigned short dataldr4[4][CANT_MEDIDAS] ={{0}};
+
 boolean flagLed1, flagLed2, flagLed3, flagLed4, termino;
 int intensidad;
 int previousMillisCell = 0;
@@ -177,12 +181,25 @@ void loop() {
 }
 
 void readldr(int led, int posled)
-{
-      analogWrite(led,intensidad);
-        dataldr[0][posled][intensidad] = analogRead(ldr1); 
-        dataldr[1][posled][intensidad] = analogRead(ldr2); 
-        dataldr[2][posled][intensidad] = analogRead(ldr3); 
-        dataldr[3][posled][intensidad] = analogRead(ldr4); 
+{    
+    int indice;
+    analogWrite(led,intensidad);
+
+    indice = (intensidad / 4) - 1;
+    dataldr1[posled][indice] = analogRead(ldr1); 
+    dataldr2[posled][indice] = analogRead(ldr2); 
+    dataldr3[posled][indice] = analogRead(ldr3); 
+    dataldr4[posled][indice] = analogRead(ldr4); 
+
+    /*
+    Serial.print("LED:  ");
+    Serial.print(posled);
+    Serial.print("  Indice  ");
+    Serial.print(indice);
+    Serial.print("  Valor ");
+    Serial.println(dataldr1[posled][indice]);
+    */
+    
     if(intensidad >= 256)  // APAGAMOS EL LED
     {
       analogWrite(led,0);
@@ -191,46 +208,70 @@ void readldr(int led, int posled)
 
 void procesar(){
   int k,j,i;
-  short media[4][4] = {{0}}, varianza[4][4] = {{0}};
+  unsigned short media1[4] = {0};
+  unsigned short media2[4] = {0};
+  unsigned short media3[4] = {0};
+  unsigned short media4[4] = {0};
+  
+  unsigned short varianza1[4] = {0};
+  unsigned short varianza2[4] = {0};
+  unsigned short varianza3[4] = {0};
+  unsigned short varianza4[4] = {0};
+
+  
+
   int promedioVar = 0;
   
   //Sumamos valores para las medias
   for(k = 0; k < 4 ; k++)
   {
+    
     for(i = 0; i < CANT_MEDIDAS; i++)
     {
-      media[0][k] = media[0][k] + dataldr[0][k][i];
-      media[1][k] = media[1][k] + dataldr[1][k][i];
-      media[2][k] = media[2][k] + dataldr[2][k][i];
-      media[3][k] = media[3][k] + dataldr[3][k][i];
-    }
+      media1[k] = media1[k] + dataldr1[k][i];
+      media2[k] = media2[k] + dataldr2[k][i];
+      media3[k] = media3[k] + dataldr3[k][i];
+      media4[k] = media4[k] + dataldr4[k][i];
+/*
+      Serial.print("LED:  ");
+    Serial.print(k);
+    Serial.print("  Indice  ");
+    Serial.print(i);
+    Serial.print("  Valor ");
+    Serial.println(dataldr1[k][i]);
+*/
 
+    }
+    //Serial.println(media1[k]);
        
     //Dividimos para obtener la media
-    media[0][k] = media[0][k] / CANT_MEDIDAS;
-    media[1][k] = media[1][k] / CANT_MEDIDAS;
-    media[2][k] = media[2][k] / CANT_MEDIDAS;
-    media[3][k] = media[3][k] / CANT_MEDIDAS;
+    media1[k] = media1[k] / CANT_MEDIDAS;
+    media2[k] = media2[k] / CANT_MEDIDAS;
+    media3[k] = media3[k] / CANT_MEDIDAS;
+    media4[k] = media4[k] / CANT_MEDIDAS;
+
+    Serial.println(media1[k]);
   }
 
-
+//Serial.println(media1[0]);
   //Calculamos las 16 varianzas
   for(k = 0; k < 4 ; k++)
   {
     for(i = 0; i < CANT_MEDIDAS; i++)
     {
-      varianza[0][k] = varianza[0][k] + ((dataldr[0][k][i] - media[0][k])*(dataldr[0][k][i] - media[0][k]));
-      varianza[1][k] = varianza[1][k] + ((dataldr[1][k][i] - media[1][k])*(dataldr[1][k][i] - media[1][k]));
-      varianza[2][k] = varianza[2][k] + ((dataldr[2][k][i] - media[2][k])*(dataldr[2][k][i] - media[2][k]));
-      varianza[3][k] = varianza[3][k] + ((dataldr[3][k][i] - media[3][k])*(dataldr[3][k][i] - media[3][k]));
+      varianza1[k] = varianza1[k] + ((dataldr1[k][i] - media1[k])*(dataldr1[k][i] - media1[k]));
+      varianza2[k] = varianza2[k] + ((dataldr2[k][i] - media2[k])*(dataldr2[k][i] - media2[k]));
+      varianza3[k] = varianza3[k] + ((dataldr3[k][i] - media3[k])*(dataldr3[k][i] - media3[k]));
+      varianza4[k] = varianza4[k] + ((dataldr4[k][i] - media4[k])*(dataldr4[k][i] - media4[k]));
     }
-    varianza[0][k] = varianza [0][k] / CANT_MEDIDAS;
-    varianza[1][k] = varianza [1][k] / CANT_MEDIDAS;
-    varianza[2][k] = varianza [2][k] / CANT_MEDIDAS;
-    varianza[3][k] = varianza [3][k] / CANT_MEDIDAS;
+    varianza1[k] = varianza1[k] / CANT_MEDIDAS;
+    varianza2[k] = varianza2[k] / CANT_MEDIDAS;
+    varianza3[k] = varianza3[k] / CANT_MEDIDAS;
+    varianza4[k] = varianza4[k] / CANT_MEDIDAS;
     
-    promedioVar = promedioVar + (varianza[0][k] + varianza[1][k] + varianza[2][k] + varianza[3][k]);
+    promedioVar = promedioVar + (varianza1[k] + varianza2[k] + varianza3[k] + varianza4[k]);
   }
-  //promedioVar = promedioVar / 16;
-  Serial.print(promedioVar); 
+  promedioVar = promedioVar / 16;
+  
+  //Serial.println(promedioVar); 
 }
